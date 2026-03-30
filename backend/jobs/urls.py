3d614e74eaa92d49ad2f2673e05fefc5ff61ed8c
Reuvi571 <reuvilipins@gmail.com>
2026-03-30 13:33:20 -0400

    Update urls.py

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/jobs", views.list_jobs),
-    path("api/jobs/create", views.create_job),
-    path("api/jobs/update/<uuid:job_id>", views.update_job),
-    path("api/jobs/delete/<uuid:job_id>", views.delete_job),
+    path('api/jobs/', views.dashboard, name='dashboard'),
+    path('api/mdn/<str:skill_name>/', views.mdn_popup, name='mdn_popup'),
 ]
